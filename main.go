b44089d7153b5439314fe0d11c5dab7a81f3c6bc
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2023-07-25 23:02:22 +0300

    Reusable layout, tailwind, navbar

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"web_dev/views"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string, data interface{}) {
@@ -37,19 +36,37 @@ type User struct {
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	// r.Use(middleware.Logger)
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
+	fileServer := http.FileServer(http.Dir("./css/"))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+	r.Handle("/css/*", http.StripPrefix("/css/", fileServer))
 
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"home.gohtml", "tailwind.gohtml",
+	))))
+
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"contact.gohtml", "tailwind.gohtml",
+	))))
+
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"faq.gohtml", "tailwind.gohtml",
+	))))
+
+	r.Get("/sign-up", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"sign-up.gohtml", "tailwind.gohtml",
+	))))
 
 	// r.Get("/user/{username}", userHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
-	fmt.Println("Starting the server on port :8080....")
+	fmt.Println("Starting the server on port :8080...")
 	http.ListenAndServe(":8080", r)
 }
