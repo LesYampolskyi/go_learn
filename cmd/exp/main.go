d9862b900dfb70555116ffc58e49c6877a7d6089
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2023-07-29 02:48:17 +0300

    Postgres playground

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -64,24 +64,90 @@ func main() {
 
 	fmt.Println("Tables created")
 
-	name := "Will"
-	email := "Wolf@test.com"
+	// name := "Will"
+	// email := "Wolf@test.com"
 
-	// _, err = db.Exec(`
+	// // _, err = db.Exec(`
+	// // 	INSERT INTO users (name, email)
+	// // 	VALUES($1, $2);
+	// // `, name, email)
+	// row := db.QueryRow(`
 	// 	INSERT INTO users (name, email)
-	// 	VALUES($1, $2);
+	// 	VALUES($1, $2) RETURNING id;
 	// `, name, email)
-	row := db.QueryRow(`
-		INSERT INTO users (name, email)
-		VALUES($1, $2) RETURNING id;
-	`, name, email)
-	// row.Err()
-	var id int
-	row.Scan(&id)
+	// // row.Err()
+	// var id int
+	// row.Scan(&id)
 
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("Users added with id = $s", id)
+
+	// id := 1
+	// row := db.QueryRow(`
+	// 	SELECT name, email
+	// 	FROM users
+	// 	WHERE id=$1
+	// `, id)
+
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("User information: name=%s email=%s\n", name, email)
+
+	// userID := 1
+	// for i := 0; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	description := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err := db.Exec(`
+	// 		INSERT INTO orders(user_id, amount, description)
+	// 		VALUES($1, $2, $3)
+	// 	`, userID, amount, description)
+
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Orders created")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+
+	var orders []Order
+
+	userID := 1
+
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		FROM orders
+		WHERE user_id=$1
+	`, userID)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+
+	err = rows.Err()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Users added with id = $s", id)
+	fmt.Println("Orders : ", orders)
 }
