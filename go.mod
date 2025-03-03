f01ce4ca293f2bd5a92b6d19f1dcc6c7bcacd1fc
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2025-03-03 22:53:01 +0200

    Clean code. TDD lesson 1

--- go.mod
+++ go.mod
@@ -1,33 +1,3 @@
 module web_dev
 
-go 1.20
-
-require (
-	github.com/go-chi/chi/v5 v5.0.10
-	github.com/gorilla/csrf v1.7.1
-	github.com/jackc/pgx/v4 v4.18.1
-	golang.org/x/crypto v0.10.0
-)
-
-require (
-	github.com/go-mail/mail/v2 v2.3.0 // indirect
-	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-)
-
-require (
-	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.1 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pressly/goose/v3 v3.14.0
-	golang.org/x/text v0.11.0 // indirect
-)
+go 1.24.0
--- main.go
+++ main.go
@@ -1,178 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
-	"web_dev/controllers"
-	"web_dev/migrations"
-	"web_dev/models"
-	"web_dev/templates"
-	"web_dev/views"
+import "fmt"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/gorilla/csrf"
-	"github.com/joho/godotenv"
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 )
 
-type User struct {
-	Name string
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
-type config struct {
-	PSQL models.PostgresConfig
-	SMTP models.SMTPConfig
-	CSRF struct {
-		Key    string
-		Secure bool
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	Server struct {
-		Address string
-	}
+	return
 }
 
-func loadEnvConfig() (config, error) {
-	var cfg config
-	err := godotenv.Load()
-	if err != nil {
-		return cfg, err
-	}
-	cfg.PSQL = models.GetDefaultPostgresConfig()
-
-	cfg.SMTP.Host = os.Getenv("SMTP_HOST")
-	cfg.SMTP.Username = os.Getenv("SMTP_USERNAME")
-	cfg.SMTP.Password = os.Getenv("SMTP_PASSWORD")
-	portStr := os.Getenv("SMTP_PORT")
-	cfg.SMTP.Port, err = strconv.Atoi(portStr)
-	if err != nil {
-		return cfg, err
-	}
-
-	cfg.CSRF.Key = "Lb38utheFHaXKAMD6pOgkAHyrPeA2nZV"
-	cfg.CSRF.Secure = false
-
-	cfg.Server.Address = ":3000"
-
-	return cfg, nil
-}
-
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/integers
 func main() {
-	cfg, err := loadEnvConfig()
-	if err != nil {
-		panic(err)
-	}
-
-	// Setup database
-
-	db, err := models.Open(cfg.PSQL)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = models.MigrateFS(db, migrations.FS, "")
-	if err != nil {
-		panic(err)
-	}
-
-	// Set up services
-	userService := &models.UserService{
-		DB: db,
-	}
-
-	sessionService := &models.SessionService{
-		DB: db,
-	}
-	pwResetService := &models.PasswordResetService{
-		DB: db,
-	}
-
-	emailService := models.NewEmailService(cfg.SMTP)
-
-	// Set up middleware
-	umw := controllers.UserMiddleware{
-		SessionService: sessionService,
-	}
-
-	crfMw := csrf.Protect(
-		[]byte(cfg.CSRF.Key),
-		csrf.Secure(cfg.CSRF.Secure),
-	)
-
-	// Set up controllers
-	usersC := controllers.Users{
-		UserService:          userService,
-		SessionService:       sessionService,
-		PasswordResetService: pwResetService,
-		EmailService:         emailService,
-	}
-
-	usersC.Template.New = views.Must(views.ParseFS(
-		templates.FS,
-		"sign-up.gohtml", "tailwind.gohtml",
-	))
-	usersC.Template.SignIn = views.Must(views.ParseFS(
-		templates.FS,
-		"sign-in.gohtml", "tailwind.gohtml",
-	))
-	usersC.Template.ForgotPassword = views.Must(views.ParseFS(
-		templates.FS,
-		"forgot-pw.gohtml", "tailwind.gohtml",
-	))
-	usersC.Template.CheckYourEmail = views.Must(views.ParseFS(
-		templates.FS,
-		"check-your-email.gohtml", "tailwind.gohtml",
-	))
-	usersC.Template.ResetPassword = views.Must(views.ParseFS(
-		templates.FS,
-		"reset-pw.gohtml", "tailwind.gohtml",
-	))
-
-	// Setup router and routes
-	r := chi.NewRouter()
-	r.Use(crfMw)
-	r.Use(umw.SetUser)
-	fileServer := http.FileServer(http.Dir("./css/"))
-	r.Handle("/css/*", http.StripPrefix("/css/", fileServer))
-
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
-		templates.FS,
-		"home.gohtml", "tailwind.gohtml",
-	))))
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
-		templates.FS,
-		"contact.gohtml", "tailwind.gohtml",
-	))))
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
-		templates.FS,
-		"faq.gohtml", "tailwind.gohtml",
-	))))
-	r.Get("/sign-up", usersC.New)
-	r.Get("/sign-in", usersC.SignIn)
-	r.Post("/sign-in", usersC.ProcessSignIn)
-	r.Post("/sign-out", usersC.ProcessingSignOut)
-	r.Get("/forgot-pw", usersC.ForgotPassword)
-	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
-	r.Get("/reset-pw", usersC.ResetPassword)
-	r.Post("/reset-pw", usersC.ProcessResetPassword)
-	r.Post("/users", usersC.Create)
-	r.Route("/users/me", func(r chi.Router) {
-		r.Use(umw.RequireUser)
-		r.Get("/", usersC.CurrentUser)
-		r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, "Hello")
-		})
-	})
-	r.Get("/users/me", usersC.CurrentUser)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page not found", http.StatusNotFound)
-	})
-
-	// Start the server
-	fmt.Printf("Starting the server on port :%s...\n", cfg.Server.Address)
-	err = http.ListenAndServe(cfg.Server.Address, r)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println(Hello("world", ""))
 }
